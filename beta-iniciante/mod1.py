19678090ded06369e1893716379168bc0fbd638d
luiz-suf <luizivanpinheiro@outlook.com>
2026-02-12 10:12:46 -0300

    Adicionando modulo 1 e 2

---
+++ beta-iniciante/mod1.py
@@ -0,0 +1,24 @@
+'''def FizzBuzz(dado):
+    if(dado % 3 == 0 and dado % 5 ==0 ):
+        print("Fizzbuzz"),
+    elif(dado % 3 ==0):
+        print("fizz"),
+    elif(dado % 5 == 0):
+        print("buzz")
+    else:print(dado) 
+
+FizzBuzz()
+        '''
+
+number = input("Digite um numero: ")
+if number == '0':
+    print('entered 0'),
+elif number == '1':
+    print('entered 1'),
+elif number == '2':
+    print("entered 2"),
+elif number == '3':
+    print(" entered 3"),
+elif number == '4':
+    print("entered 4"),
+else: print("number invalide") 
