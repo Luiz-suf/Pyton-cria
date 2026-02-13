8880846a1a19c2440f4e52670f3f85e25df76232
luiz-suf <luizivanpinheiro@outlook.com>
2026-02-13 02:28:38 -0300

    add mais exercicios

---
+++ exerciciospy/ex037.py
@@ -0,0 +1,7 @@
+num1 = int(input("Digite um numero: "))
+num2 = int(input("Digite outro numero: "))
+if num1 > num2:
+    print(f"Valo {num1} é maior que num {num2}"),
+elif num2 > num1 :
+    print(f"Valo {num2} é maior que {num1}"),
+else: print("NAO EXISTE VALOR MAIOR OS DOIS SAO IGUAIS")
---
+++ exerciciospy/ex038.py
@@ -0,0 +1,7 @@
+HoraDoAlistamento = int(input("Digite o ano do sue nascimento"))
+if(HoraDoAlistamento > 2008):
+    print("TA PERTO DO CAPETA")
+elif(HoraDoAlistamento == 2008):
+    print("Va se alistar agora seu bisonho")
+elif(HoraDoAlistamento < 2008):
+    print("ja passou da hora seu maldito")
